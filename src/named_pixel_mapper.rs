38db2582ff2d17aa36a225c94746b8eea4218480
Kentaro Okuda <lonesometraveler@mac.com>
2024-02-27 15:35:29 -0500

    Add Reversed U mapper

--- src/named_pixel_mapper.rs
+++ src/named_pixel_mapper.rs
@@ -43,6 +43,17 @@ pub enum NamedPixelMapperType {
     ///   [<][<][<][<]  }--- Pi connector #2
     ///   [>][>][>][>]
     UMapper,
+    /// The `ReversedUMapper` represents a pixel mapping strategy where a long chain of display panels
+    /// is arranged in a U-shape configuration. This arrangement allows for a single chain display
+    ///
+    /// with panels of double height but still utilizing only one data chain.
+    /// This Reversed U-shape configuration can also be applied to displays with multiple chains.
+    /// For instance, an arrangement with two chains, each consisting of 8 panels, can be represented as follows:
+    ///   Pi connector #1 --} [>][>][>][>]
+    ///                       [<][<][<][<]
+    ///   Pi connector #2 --} [>][>][>][>]
+    ///                       [<][<][<][<]
+    ReversedUMapper,
 }
 
 impl FromStr for NamedPixelMapperType {
@@ -77,6 +88,8 @@ impl FromStr for NamedPixelMapperType {
             }
         } else if s == "U-mapper" {
             Ok(Self::UMapper)
+        } else if s == "R-U-mapper" {
+            Ok(Self::ReversedUMapper)
         } else {
             Err(format!("'{}' is not a valid Pixel mapping.", s).into())
         }
@@ -90,6 +103,9 @@ impl NamedPixelMapperType {
             NamedPixelMapperType::Rotate(angle) => Box::new(RotatePixelMapper { angle }),
             NamedPixelMapperType::UMapper => {
                 Box::new(UArrangeMapper::new_with_parameters(chain, parallel))
+            }
+            NamedPixelMapperType::ReversedUMapper => {
+                Box::new(ReversedUArrangeMapper::new_with_parameters(chain, parallel))
             }
         }
     }
@@ -214,6 +230,61 @@ impl NamedPixelMapper for UArrangeMapper {
         } else {
             // Lower panel of the slab
             [(visible_width - x - 1), (slab_height - y_in_slab - 1)]
+        };
+
+        [matrix_x, base_y + matrix_y]
+    }
+}
+
+struct ReversedUArrangeMapper {
+    parallel: usize,
+}
+
+impl ReversedUArrangeMapper {
+    fn new_with_parameters(chain: usize, parallel: usize) -> Self {
+        if chain < 2 {
+            // technically, a chain of 2 would work, but somewhat pointless
+            panic!("U-mapper: need at least '--chain_length 4' for useful folding");
+        }
+        if chain % 2 != 0 {
+            panic!("U-mapper: Chain (--chain_length) needs to be divisible by two");
+        }
+        Self { parallel }
+    }
+}
+
+impl NamedPixelMapper for ReversedUArrangeMapper {
+    fn get_size_mapping(&self, matrix_width: usize, matrix_height: usize) -> [usize; 2] {
+        let visible_width = (matrix_width / 64) * 32; // Div at 32px boundary
+        let visible_height = 2 * matrix_height;
+        if matrix_height % self.parallel != 0 {
+            eprintln!(
+                "For parallel={} we would expect the height={matrix_height} to be divisible by {}.",
+                self.parallel, self.parallel
+            );
+        }
+        [visible_width, visible_height]
+    }
+
+    fn map_visible_to_matrix(
+        &self,
+        matrix_width: usize,
+        matrix_height: usize,
+        x: usize,
+        y: usize,
+    ) -> [usize; 2] {
+        let panel_height = matrix_height / self.parallel;
+        let visible_width = (matrix_width / 64) * 32;
+        let slab_height = 2 * panel_height; // one folded u-shape
+        let base_y = (y / slab_height) * panel_height;
+        let y_in_slab = y % slab_height;
+
+        let [matrix_x, matrix_y] = if y_in_slab < panel_height {
+            // Upper panel of the slab
+            [(x + matrix_width / 2), panel_height - y_in_slab - 1]
+        } else {
+            // Lower panel of the slab
+            [(visible_width - x - 1), (y_in_slab - panel_height)]
         };
 
         [matrix_x, base_y + matrix_y]
